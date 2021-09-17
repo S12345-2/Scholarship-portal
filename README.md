8ae03d9e9891076c01e4d5a30ff9cd016807cacb
sana ullah <44126029+S12345-2@users.noreply.github.com>
2021-09-17 16:12:40 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Scholarship-portal
+## ScholarshipPortal-dbms-project
+
+An Online Scholarship Portal is an online platform where student can fill their
+details in an application on the go and also searches for scholarships to apply easily.
+<br>
+At Admin page one can upload the scholarship details available to apply given by the
+donor organisations so that they can work upon the suggestions and improvement can
+be done on the project, also they can upload scholarship awardee details. Usually, the
+user will be asked to select scholarship as per the eligibility required, and the same is
+recorded in the database. A notification is sent to the user as soon as the record is
+being saved.
+
+<b>3.1.2 Software Configuration:<b><br>
+
+ 👉  <b>Database :MySql<br>
+ 👉 <b>Server :Xampp<br>
+ 👉 <b>Front End : HTML,CSS,JavaScript<br>
+ 👉 <b>Back End : PHP
+
---
+++ aboutus.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<title>ABOUT US</title>
+<style>
+.button {
+    background-color:#99ffff;
+    border: none;
+    color:golden;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 10%
+}
+body {
+background-image: url("yy.jpg");
+
+background-size: cover;
+}
+</style>
+
+	 <button class="button" value="home" onClick="window.location.href='homepage1.php'" class="css3button" style="margin-left: 40px"  >HOME</button>
+
+	    <h2 align="center"><font color="#191970" size="20" face="segeo print">ABOUT US</font> </h2>
+		<P align="center"> Department of Computer System Engineering <br>
+		                  NED University of Engineering & Technology karachi <br>
+						   SANA ULLAH<br> 
+                           SHOAIB<br>						   
+</head>
+</html>
